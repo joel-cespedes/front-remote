8da17916cd79c5da41e5cf172a25f4e036276e55
Joel Cespedes <joelguitar@gmail.com>
2026-02-08 21:31:33 +0100

    dddd

---
+++ api/angular-client/src/models/index.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ApiResponse1';
+export * from './DocumentType';
+export * from './ErrorResponse';
+export * from './ErrorResponseErrorsInner';
+export * from './Execution';
+export * from './ExecutionGeneratedFilesInner';
+export * from './ExecutionStatus';
+export * from './ExecutionsGet200Response';
+export * from './LaunchExecutionRequest';
+export * from './Metadata';
+export * from './ModelApiResponse';
+export * from './Template';
+export * from './TemplateCategory';
+export * from './TemplateStatus';
+export * from './TemplatesGet200Response';
